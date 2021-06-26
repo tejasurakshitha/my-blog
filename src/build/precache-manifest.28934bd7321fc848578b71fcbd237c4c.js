db29a0d4b28512a8463ac9dd173fa3f1a1c02648
tejasurakshitha <77142425+tejasurakshitha@users.noreply.github.com>
2021-06-26 07:16:56 +0530

    First commit

---
+++ src/build/precache-manifest.28934bd7321fc848578b71fcbd237c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5306cb9593f7cc2a738",
+    "url": "/static/css/main.75acc8d6.chunk.css"
+  },
+  {
+    "revision": "c5306cb9593f7cc2a738",
+    "url": "/static/js/main.8c51674b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31aad6241f464ce200dd",
+    "url": "/static/js/2.7e0ae54e.chunk.js"
+  },
+  {
+    "revision": "c839551163fde87bf2db55f5a0afa8b8",
+    "url": "/index.html"
+  }
+];
